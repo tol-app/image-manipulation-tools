5f9765955a32914189c0130658ffe243f9a2fcfb
Erick <74315338+erickahmed@users.noreply.github.com>
2022-06-30 19:46:47 +0200

    Remove unused dependencies

--- src/img-resizer/img-resizer.py
+++ src/img-resizer/img-resizer.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import os
-from matplotlib import pyplot as plt
 
 inputFolder  = os.path.join(os.getcwd(), 'dataset', 'dataset-original')
 outputFolder = os.path.join(os.getcwd(), 'dataset', 'dataset-resized')
