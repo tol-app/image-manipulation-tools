03b03ad1fbec410487b8b8bd90d555f84f8297d0
Erick <ahmederick00@gmail.com>
2022-06-30 18:14:11 +0200

    Fixed path reading bugs

--- src/img-resizer/img-resizer.py
+++ src/img-resizer/img-resizer.py
@@ -3,22 +3,20 @@ import cv2
 import os
 from matplotlib import pyplot as plt
 
-inputFolder  = 'dataset/dataset-original'
-outputFolder = 'dataset/dataset-resized'
+inputFolder  = os.path.join(os.getcwd(), 'dataset', 'dataset-original')
+outputFolder = os.path.join(os.getcwd(), 'dataset', 'dataset-resized')
 resolution   = tuple((640,640))
 
-if os.path.exists(outputFolder) is True:
-    try:
-        os.mkdir(outputFolder)
-    except AssertionError:
-            print ("Output path does not exist")
+if os.path.exists(outputFolder) is False:
+    os.mkdir(outputFolder)
 
 for filename in os.listdir(inputFolder):
     if filename.endswith(".jpg") or filename.endswith(".jpeg"):
-        img_org = cv2.imread(filename)
+        img_org = cv2.imread(inputFolder + '/' + filename)
         img_res = cv2.resize(img_org, resolution)
         cv2.imwrite(os.path.join(outputFolder, filename), img_res)
     else:
         continue
 
+#debug string
 print(cv2.__version__)
